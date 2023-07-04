df1672dc2b4e54961d3eec99268fd9d1db68c674
AmrAbdoElshafii <137432578+AmrAbdoElshafii@users.noreply.github.com>
2023-07-04 14:16:03 +0300

    empty

---
+++ index.js
@@ -0,0 +1,5 @@
+var myTitle = 'elzero', myDescription = 'elzero web school', myDate = '25/10';
+
+var page = 
+`<div> <h3> Hello ${myTitle} <h3> <p> ${myDescriptio} </p> <span> ${myDate} </span></div>`;
+document.write(page);
